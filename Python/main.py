314db4543e06c2ed126dbb97d32589ae84e71a15
maxence-lh <lh.maxence@live.fr>
2021-11-03 09:35:24 +0100

    update main and schéma

--- Python/main.py
+++ Python/main.py
@@ -2,6 +2,7 @@
 # create, read, update, delete
 
 import mysql.connector
+from Python.warehouse import Warehouses
 from employees import Employees
 
 def format_texte(liste):
@@ -25,7 +26,7 @@ while True:
     choix = input("Que voulez vous ? : ")
 
     if choix == "1":
-        test = Employees(a)
+        test = Warehouses(a)
         while True:
             print("1 - Voir les employés")
             print("2 - Ajouter un employé")
@@ -64,6 +65,40 @@ while True:
     elif choix == "2":
         pass
     elif choix == "3":
-        pass
+        test = Employees(a)
+        while True:
+            print("1 - Voir les entrepôts")
+            print("2 - Ajouter un entrepôts")
+            print("3 - Supprimer un entrepôts")
+            print("4 - Modifier les données d'un entrepôts")
+            print("99 - retour")
+            choix_emp = input("Que voulez-vous ? : ")
+
+            if choix_emp == "1":
+                print(format_texte(test.get_information()))
+                break
+            elif choix_emp == "2":
+                lastname_emp = input("Lastname : ")
+                firstname_emp = input("Firstname : ")
+                age_emp = input("Age : ")
+                jobsid_emp = input("jobs_id : ")
+                civilityid_emp = input("civlity_id : ")
+                warehouseid_emp = input("warehouse_id : ")
+                test.create(lastname_emp, firstname_emp, age_emp, jobsid_emp, civilityid_emp, warehouseid_emp)
+                print("Vous avez créé un employé")
+                break
+            elif choix_emp == "3":
+                id_emp = input("id de l'employé")
+                test.delete(id_emp)
+                print("Vous avez supprimé un employé")
+                break
+            elif choix_emp == "4":
+                champ_emp = input("Champ à modifier : ")
+                valeur_emp = input("Valeur du champ : ")
+                id_emp = input("Id de l'employé : ")
+                test.modify(champ_emp, valeur_emp, id_emp)
+                break
+            elif choix_emp == "99":
+                break
     elif choix == "99":
         break
