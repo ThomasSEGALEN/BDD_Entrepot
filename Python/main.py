dee4b8a805b33a50a68b7b6b5c35d59ee3799e28
MathisD49 <mathisdubuc@gmail.com>
2021-11-03 08:54:54 +0100

    création d'un menu pour intéragir avec l'objet employees

--- Python/main.py
+++ Python/main.py
@@ -6,18 +6,66 @@ from employees import Employees
 
 a = mysql.connector.connect(host='localhost', database='mydb', user='root', password='')
 
-test = Employees(a)
+# print(test.get_information())
 
-print(test.get_information())
+# # test.create("Dubuc", "Mathis", 19, 1, 1, 1)
 
-# test.create("Dubuc", "Mathis", 19, 1, 1, 1)
+# # print(test.get_information())
+
+# test.delete(1)
+
+# # test.modify("age", 41, 1)
 
 # print(test.get_information())
 
-test.delete(1)
+# # print(a)
 
-# test.modify("age", 41, 1)
+while True:
+    print("1 - employés")
+    print("2 - produits")
+    print("3 - entrepot")
+    print("99 - quitter")
+    choix = input("Que voulez vous ? : ")
 
-print(test.get_information())
+    if choix == "1":
+        test = Employees(a)
+        while True:
+            print("1 - Voir les employés")
+            print("2 - Ajouter un employé")
+            print("3 - Supprimer un employé")
+            print("4 - Modifier les données d'un employé")
+            print("99 - retour")
+            choix_emp = input("Que voulez-vous ? : ")
 
-# print(a)
+            if choix_emp == "1":
+                print(test.get_information())
+                break
+            elif choix_emp == "2":
+                lastname_emp = input("Lastname : ")
+                firstname_emp = input("Firstname : ")
+                age_emp = input("Age : ")
+                jobsid_emp = input("jobs_id : ")
+                civilityid_emp = input("civlity_id : ")
+                warehouseid_emp = input("warehouse_id : ")
+                test.create(lastname_emp, firstname_emp, age_emp, jobsid_emp, civilityid_emp, warehouseid_emp)
+                print("Vous avez créé un employé")
+                break
+            elif choix_emp == "3":
+                id_emp = input("id de l'employé")
+                test.delete(id_emp)
+                print("Vous avez supprimé un employé")
+                break
+            elif choix_emp == "4":
+                champ_emp = input("Champ à modifier : ")
+                valeur_emp = input("Valeur du champ : ")
+                id_emp = input("Id de l'employé : ")
+                test.modify(champ_emp, valeur_emp, id_emp)
+                break
+            elif choix_emp == "99":
+                break
+    elif choix == "2":
+        pass
+    elif choix == "3":
+        pass
+    elif choix == "99":
+        break
