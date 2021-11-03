801c54d4056f0f1f76910c13824f814e02d235fc
maxence-lh <lh.maxence@live.fr>
2021-11-03 09:25:26 +0100

    add shéma and update warehouse.py

--- Python/warehouse.py
+++ Python/warehouse.py
@@ -1,17 +1,30 @@
-import mysql.connector
+class Warehouses:
+    def __init__(self, myDB):
+        self.myDB = myDB
+        self.cursor = self.myDB.cursor()
+    
+    def get_information(self):
+        result = []
+        query = "SELECT * FROM warehouses;"
+        self.cursor.execute(query)
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="test",
-    password="test",
-    database="mydb"
-)
+        for (id, name, city, quantity_product_max, slot_id) in self.cursor:
+            result.append([id, name, city, quantity_product_max, slot_id])
 
-mycursor = mydb.cursor()
+        return result
+    
+    def create(self, name, city, quantity_product_max, slot_id):
+        query = ("INSERT INTO warehouses(lastname, firstname, age, jobs_id, civility_id, warehouse_id) VALUES (%s, %s, %s, %s, %s, %s);")
+        self.cursor.execute(query, (name, city, quantity_product_max, slot_id))
+        self.myDB.commit()
 
-mycursor.execute("SELECT * FROM warehouses")
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-    print(x)
+    def delete(self, id):
+        print(self.get_information())
+        query = ("DELETE FROM warehouses WHERE id=%s;")
+        self.cursor.execute(query, (id,))
+        self.myDB.commit()
+    
+    def modify(self, champ, valeur, id):
+        query = ("UPDATE warehouses SET " + champ + " = %s WHERE id = %s;")
+        self.cursor.execute(query, (valeur, id))
+        self.myDB.commit()
