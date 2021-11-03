21b112d68de121e8de33b124dc202eda55a44b51
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2021-11-03 10:10:22 +0100

    push

--- Python/products.py
+++ Python/products.py
@@ -30,49 +30,49 @@ select(args, table)
 
 
 # INSERT QUERY
-# def insert():
-#     query = ('INSERT INTO products(name, description, price, quantity, weight, height, category_id) VALUES (%s, %s, %s, %s, %s, %s, %s)')
-#     name = input('name: ')
-#     description = input('description: ')
-#     price = input('price: ')
-#     quantity = input('quantity: ')
-#     weight = input('weight: ')
-#     height = input('height: ')
-#     category_id = input('category_id: ')
-#     data = (name, description, price, quantity, weight, height, category_id)
-#     cursor.execute(query, data)
-#     connection.commit()
-# print('-- Insert query --')
+def insert():
+    query = ('INSERT INTO products(name, description, price, quantity, weight, height, category_id) VALUES (%s, %s, %s, %s, %s, %s, %s)')
+    name = input('name: ')
+    description = input('description: ')
+    price = input('price: ')
+    quantity = input('quantity: ')
+    weight = input('weight: ')
+    height = input('height: ')
+    category_id = input('category_id: ')
+    data = (name, description, price, quantity, weight, height, category_id)
+    cursor.execute(query, data)
+    connection.commit()
+print('-- Insert query --')
 # insert()
 
 
 # UPDATE QUERY
-# def update(table, column):
-#     query = ('UPDATE ' + table + ' SET ' + column + ' = %s WHERE id = %s')
-#     id = input('id: ')
-#     value = input('value: ')
-#     cursor.execute(query, (value, id))
-#     connection.commit()
-# print('-- Update query --')
-# table = input('table: ')
-# column = input('column: ')
-# if table == '':
-#     table = 'products'
+def update(table, column):
+    query = ('UPDATE ' + table + ' SET ' + column + ' = %s WHERE id = %s')
+    id = input('id: ')
+    value = input('value: ')
+    cursor.execute(query, (value, id))
+    connection.commit()
+print('-- Update query --')
+table = input('table: ')
+column = input('column: ')
+if table == '':
+    table = 'products'
 # update(table, column)
 
 
 # DELETE QUERY
-# def delete(table, column):
-#     query = ('DELETE FROM ' + table + ' WHERE ' + column + '= %s')
-#     value = input('value: ')
-#     cursor.execute(query, (value,))
-#     connection.commit()
-#     print('Number of rows deleted:', cursor.rowcount)
-# print('-- Delete query --')
-# table = input('table: ')
-# column = input('column: ')
-# if table == '':
-#     table = 'products'
+def delete(table, column):
+    query = ('DELETE FROM ' + table + ' WHERE ' + column + '= %s')
+    value = input('value: ')
+    cursor.execute(query, (value,))
+    connection.commit()
+    print('Number of rows deleted:', cursor.rowcount)
+print('-- Delete query --')
+table = input('table: ')
+column = input('column: ')
+if table == '':
+    table = 'products'
 # delete(table, column)
 
 
